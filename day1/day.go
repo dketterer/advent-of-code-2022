94ae217d622aeab3303252ccdf00ddc73c49f5ed
Daniel Ketterer <26778610+dketterer@users.noreply.github.com>
2022-12-01 17:53:36 +0100

    :star: :star: solve day1

--- day1/day.go
+++ day1/day.go
@@ -2,6 +2,8 @@ package day1
 
 import (
 	_ "embed"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -9,17 +11,37 @@ import (
 var input string
 
 type D struct {
+	calories []int
 }
 
 func (d *D) Name() string {
-	return "--- Day 1:  ---"
+	return "--- Day 1: Calorie Counting ---"
 }
 
 func (d *D) Part1() (_ int, err error) {
 	lines := strings.Split(input, "\n")
-	return len(lines), err
+	d.calories = []int{0}
+
+	for _, line := range lines {
+		if len(line) == 0 {
+			d.calories = append(d.calories, 0)
+			continue
+		}
+		cal, _ := strconv.Atoi(line)
+		d.calories[len(d.calories)-1] += cal
+	}
+
+	sort.Ints(d.calories)
+
+	return d.calories[len(d.calories)-1], err
 }
 
 func (d *D) Part2() (int, error) {
-	return 0, nil
+	var result int
+
+	result += d.calories[len(d.calories)-1]
+	result += d.calories[len(d.calories)-2]
+	result += d.calories[len(d.calories)-3]
+
+	return result, nil
 }
